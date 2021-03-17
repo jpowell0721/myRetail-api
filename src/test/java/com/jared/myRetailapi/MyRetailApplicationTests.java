efec480a2f6e34a7cb1a2e8ea885a0480b70f503
Jared Powell <jaredpowell@Jareds-MacBook-Pro.local>
2021-03-16 23:06:37 -0500

    Initial commit

---
+++ src/test/java/com/jared/myRetailapi/MyRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jared.myRetailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
